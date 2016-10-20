b2968c56df2f1d9ef65967525fac947d8da0ca41
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-20 17:15:20 -0400

    Complete

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,24 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
+    @IBOutlet weak var fruitPicker: UIPickerView!
+    
+
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fruitPicker.dataSource = self
+        fruitPicker.delegate = self
+        
+        
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        
+        
         
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
+        didWin()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func didWin() {
+        var results = Set<Int>()
+        var fruitCombo = [String]()
+        var didWin: Bool {
+            return results.count == 1
+        }
         
-        
-        
+        for num in 0..<3 {
+            let randomNum = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+            fruitPicker.selectRow(randomNum, inComponent: num, animated: true)
+            results.insert(randomNum)
+            fruitCombo.append(fruitsArray[randomNum])
+            
+        }
+        print(results)
+        print(fruitCombo)
+        resultLabel.alpha = 1
+        switch didWin {
+        case true:
+                resultLabel.text = "WINNER!"
+                UIView.animate(withDuration: 1, delay: 0, options: .repeat, animations: {
+                    self.resultLabel.alpha = 0
+                    }, completion: nil)
+        default:
+                resultLabel.text = "TRY AGAIN"
+                UIView.animate(withDuration: 1) {
+                    self.resultLabel.alpha = 0
+            }
+        }
     }
 
 }
--- Podfile
+++ Podfile
@@ -1,8 +1,8 @@
-# Uncomment this line to define a global platform for your project
+# Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
 target 'PickinFruit' do
-  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for PickinFruit
